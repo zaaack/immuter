5b5e20da932df0df3f02511bd4a97e404926621e
zaaack <zaaack@qq.com>
2017-07-11 20:33:49 +0800

    Refactor

--- src/struct.js
+++ src/struct.js
@@ -1,26 +1,35 @@
 // @flow
-import { get as _get, fromPairs, isPlainObject } from 'lodash'
+import { get as _get, fromPairs } from 'lodash'
 import { fpSet } from './common'
 
 function getType(obj: any) {
   return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
 }
-const STRUCT_CONTEXT = '__structContext'
+const STRUCT_CONTEXT = typeof Symbol === 'function' ? Symbol('__structContext') : '__structContext'
 const primitiveTypes = fromPairs([
   'boolean', 'string', 'number', 'null', 'undefined',
   'array', 'map', 'set', 'function', 'date', 'regexp',
 ].map(i => [i, true]))
 
 export default function Struct<T: Object>(state: T): T {
-  return _Struct(state)
+  const ctx = {
+    state: {...state},
+  }
+  // Fix ie polyfill
+  Object.defineProperty(ctx.state, STRUCT_CONTEXT, {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: ctx,
+  })
+  return (new Proxy(ctx.state, makeHandler({ctx})): any)
 }
-
 Struct.clone = function<T: Object> (struct: T): T {
   const ctx = struct[STRUCT_CONTEXT]
   if (!ctx) {
-    return _Struct(struct)
+    return Struct(struct)
   }
-  return _Struct(ctx.state, ctx.newState)
+  return Struct(ctx.state)
 }
 
 Struct.isStruct = function (struct: any) {
@@ -31,26 +40,18 @@ Struct.isStruct = function (struct: any) {
 }
 
 function makeHandler({ctx, chain = []}: {
-  ctx: {state: any, newState: any},
+  ctx: {state: any},
   chain?: string[]
 }) {
   return {
     get(target: any, property: string, receiver: any) {
-      // console.log('get', property, /*ctx.state, ctx.newState, ctx*/)
+      // console.log('get', property, /*ctx.state, ctx*/)
       if (target === ctx.state && property === STRUCT_CONTEXT) {
         // console.log('get instance', STRUCT_INSTANCE, ctx)
         return ctx
       }
       const _chain = chain.concat(property)
-      let val = _get(ctx.newState, _chain)
-      if (typeof val === 'undefined') {
-        val = _get(ctx.state, _chain)
-      } else if (isPlainObject(val) || (Struct.isStruct(val))) {
-        val = {
-          ...(_get(ctx.state, _chain) || {}),
-          ...val,
-        }
-      }
+      let val = _get(ctx.state, _chain)
       // console.log('val before scalar', _chain, val)
       if (getType(val) in primitiveTypes) {
         return val
@@ -60,23 +61,8 @@ function makeHandler({ctx, chain = []}: {
     },
     set(target, property, value, receiver) {
       // console.log('set', chain, property, value)
-      ctx.newState = fpSet(ctx.newState, chain.concat(property), value)
+      ctx.state = fpSet(ctx.state, chain.concat(property), value)
       return true
     },
   }
-}
-
-function _Struct<T: Object>(state: T, newState?: Object): T {
-  const ctx = {
-    state: {...state},
-    newState: newState || {},
-  }
-  // Fix ie polyfill
-  Object.defineProperty(ctx.state, STRUCT_CONTEXT, {
-    enumerable: false,
-    configurable: true,
-    writable: true,
-    value: ctx,
-  })
-  return (new Proxy(ctx.state, makeHandler({ctx})): any)
 }
